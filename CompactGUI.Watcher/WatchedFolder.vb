21008bbe6029625bb93fd57826be60fb7d40c125
marcmy <21000174+marcmy@users.noreply.github.com>
2026-08-26 19:15:49 -0400

    Persist watcher skip-list enabled state

--- CompactGUI.Watcher/WatchedFolder.vb
+++ CompactGUI.Watcher/WatchedFolder.vb
@@ -21,6 +21,7 @@ Public Class WatchedFolder
     <ObservableProperty> Private _LastCheckedSize As Long
     <ObservableProperty> Private _CompressionLevel As Core.WOFCompressionAlgorithm
     <ObservableProperty> Private _SkipList As List(Of String)
+    <ObservableProperty> Private _SkipListEnabled As Boolean = True
 
     <AttachAttribute(GetType(JsonIgnoreAttribute))>
     <ObservableProperty> Private _IsWorking As Boolean
